75ce0d853e0ecb3b81c11b3ad4648f7385ba7883
khs5696 <ghs130731@naver.com>
2021-11-11 17:18:14 +0000

    swap-anon / iter 해결 (swap-fork 보류)

--- include/vm/anon.h
+++ include/vm/anon.h
@@ -2,6 +2,7 @@
 #define VM_ANON_H
 #include "vm/vm.h"
 #include "threads/synch.h"
+#include <bitmap.h>
 // #include "userprog/process.h"
 
 struct page;
@@ -16,8 +17,9 @@ struct anon_page {
 
 struct swap_table {
 	struct lock lock;
-	struct bitmap * bitmap;
+	struct bitmap * bit_map;
 };
+
 
 void vm_anon_init (void);
 bool anon_initializer (struct page *page, enum vm_type type, void *kva);
